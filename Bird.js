59a6b59373f874522c5c73de8ceb2a4a89f60d18
saanhvisrivastava <saanhvisrivastava@gmail.com>
2020-06-08 18:28:58 +0530

    bugs solved

--- Bird.js
+++ Bird.js
@@ -11,6 +11,7 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+    console.log(this.body.speed);
 
     if(this.body.velocity.x > 10 && this.body.position.x > 200){
       var position = [this.body.position.x, this.body.position.y];
